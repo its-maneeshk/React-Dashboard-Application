23256164832e7a41d8baf87154a61f5b9574d8db
Manish Patel <maneeshkurmii@gmail.com>
2026-01-04 04:51:26 -0600

    fix: adding about route

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Home from "./pages/Home.jsx";
+import Data from "./pages/Data.jsx";
+import About from "./pages/About.jsx";
 
-const App = () => {
+function App() {
   return (
     <Router>
       <div className="flex min-h-screen bg-gray-100">
         <Sidebar />
         <div className="flex-1 p-6">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
+            <Route
+              path="/"
+              element=
+              {
+                <Home />
+              }
+            />
+            <Route
+              path="/data"
+              element=
+              {
+                <Data />
+              }
+            />
+            <Route
+              path="/about"
+              element=
+              {
+                <About />
+              }
+            />
           </Routes>
         </div>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
